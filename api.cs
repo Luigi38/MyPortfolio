6ef50a68004c1efc7f7c5194bb8cb22066a2092f
MineEric64 <besteric40@gmail.com>
2021-01-06 01:38:41 +0900

    Add files via upload

---
+++ api.cs
@@ -0,0 +1,182 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendAnywhere
+{
+    public class SendClass
+    {
+        private int key = -1;
+
+        private List<string> filePaths = new List<string>();
+        private Dictionary<string, List<Dictionary<string, object>>> requests_payload = new Dictionary<string, List<Dictionary<string, object>>>();
+        private Dictionary<string, string> file_names = new Dictionary<string, string>();
+
+        private const string default_device_key = "MY_DEFAULT_DEVICE_KEY";
+        private string device_key = default_device_key;
+
+        private string session_start_link = string.Empty;
+
+        public SendClass(string[] filePaths)
+        {
+            Initialize(filePaths, default_device_key);
+        }
+
+        public SendClass(string[] filePaths, string deviceKey)
+        {
+            Initialize(filePaths, deviceKey);
+        }
+
+        private void Initialize(string[] filePaths, string deviceKey)
+        {
+            this.filePaths.AddRange(filePaths);
+            this.device_key = deviceKey;
+
+            foreach (string x in this.filePaths)
+            {
+                Dictionary<string, object> info = new Dictionary<string, object>();
+                info.Add("name", Path.GetFileName(x));
+                info.Add("size", GetFileSize(x));
+
+                if (!this.requests_payload.ContainsKey("file"))
+                {
+                    this.requests_payload.Add("file", new List<Dictionary<string, object>>());
+                }
+                this.requests_payload["file"].Add(info);
+
+                this.file_names.Add(Path.GetFileName(x), x);
+            }
+        }
+
+        public async Task<int> SendAsyncWithKey()
+        {
+            await SendAsync();
+            return this.key;
+        }
+
+        public async Task<string> SendAsyncWithKeyToString()
+        {
+            await SendAsync();
+            return GetKeyToString();
+        }
+
+        private async Task<Dictionary<string, string>> SendAsync()
+        {
+            string payload_json = DictionaryToJson(this.requests_payload);
+            HttpClient httpClient = new HttpClient();
+
+            StringContent payload = new StringContent(payload_json, Encoding.UTF8, "application/json");
+            UriBuilder baseUri = new UriBuilder("https://send-anywhere.com/web/key");
+            baseUri.Query = "device_key=" + this.device_key;
+
+            HttpResponseMessage req = await httpClient.PostAsync(baseUri.Uri, payload);
+            string json = await req.Content.ReadAsStringAsync();
+            Dictionary<string, string> dict = JsonToDictionary<string, string>(json);
+
+            string link = dict["weblink"];
+
+            this.key = Convert.ToInt32(dict["key"]);
+            this.session_start_link = link.Substring(0, link.IndexOf("/api/") + 5) + "session_start/" + GetKeyToString();
+
+            return dict;
+        }
+
+        public async Task FetchAsync()
+        {
+            try
+            {
+                string session_link = ReplaceFirst(this.session_start_link, "/session_start/", "/session/");
+                string session_finish_link = ReplaceFirst(this.session_start_link, "/session_start/", "/session_finish/");
+                string data_upload_link = session_link.Substring(0, session_link.IndexOf("/session/") + 9) + "file/";
+
+                string payload_json = DictionaryToJson(this.requests_payload);
+                HttpClient httpClient = new HttpClient();
+
+                StringContent payload = new StringContent(payload_json, Encoding.UTF8, "application/json");
+                UriBuilder baseUri = new UriBuilder(session_start_link);
+                baseUri.Query = "device_key=" + this.device_key;
+
+                HttpResponseMessage req = await httpClient.PostAsync(baseUri.Uri, payload);
+                string json = await req.Content.ReadAsStringAsync();
+                var key_data = JsonToDictionary<string, object>(json);
+
+                foreach (var x in (Newtonsoft.Json.Linq.JArray)key_data["file"])
+                {
+                    Dictionary<string, string> file = new Dictionary<string, string>();
+                    file.Add("key", x.ToObject<Dictionary<string, object>>()["key"].ToString());
+                    file.Add("name", x.ToObject<Dictionary<string, object>>()["name"].ToString());
+                    file.Add("path", this.file_names[file["name"]]);
+                    byte[] file_data = File.ReadAllBytes(file["path"]);
+
+                    baseUri = new UriBuilder(data_upload_link + file["key"]);
+                    baseUri.Query = "device_key=" + this.device_key + "&offset=0";
+
+                    MultipartFormDataContent fileContent = new MultipartFormDataContent("Upload----" + DateTime.Now.ToString(CultureInfo.InvariantCulture));
+                    fileContent.Add(new StreamContent(new MemoryStream(file_data)), file["name"], file["name"]);
+
+                    await httpClient.PostAsync(baseUri.Uri, fileContent);
+                }
+
+                //session end
+                baseUri = new UriBuilder(session_link);
+                baseUri.Query = "device_key=" + this.device_key + "&mode=status&_=" + DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+                await httpClient.GetAsync(baseUri.Uri);
+
+                baseUri = new UriBuilder(session_finish_link);
+                baseUri.Query = "device_key=" + this.device_key + "&mode=status&_=" + DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+                await httpClient.GetAsync(baseUri.Uri);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        public string GetKeyToString()
+        {
+            return this.key.ToString("D6");
+        }
+
+        /// <summary>
+        /// Convert Dictionary Type to Json Type.
+        /// </summary>
+        /// <param name="dict"></param>
+        /// <returns></returns>
+        private string DictionaryToJson<TKey, TValue>(Dictionary<TKey, TValue> dict)
+        {
+            return JsonConvert.SerializeObject(dict);
+        }
+
+        /// <summary>
+        /// Convert Json Type To Dictionary Type
+        /// </summary>
+        /// <param name="dict"></param>
+        /// <returns></returns>
+        private Dictionary<TKey, TValue> JsonToDictionary<TKey, TValue>(string json)
+        {
+            return JsonConvert.DeserializeObject<Dictionary<TKey, TValue>>(json);
+        }
+
+        private string ReplaceFirst(string text, string search, string replace)
+        {
+            int pos = text.IndexOf(search);
+            if (pos < 0)
+            {
+                return text;
+            }
+            return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+        }
+
+        private long GetFileSize(string path)
+        {
+            return new FileInfo(path).Length;
+        }
+    }
+}
---
+++ api2.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SendAnywhere
+{
+    public class RecieveClass
+    {
+        /// <summary>
+        /// key
+        /// </summary>
+        private int key;
+
+        private const DEVICE_KEY = "MY_DEVICE_KEY";
+        private const ACCESS_TOKEN = "MY_ACCESS_TOKEN";
+
+        /// <summary>
+        /// Initialize
+        /// </summary>
+        /// <param name="key">key</param>
+        public RecieveClass(int key)
+        {
+            this.key = key;
+        }
+
+        /// <summary>
+        /// Get response data with dictionary using key for asynchronous.
+        /// </summary>
+        /// <returns>Dictionary</returns>
+        private async Task<Dictionary<string, string>> GetResponseAsync()
+        {
+            const string COOKIE_VALUE = $"device_key={DEVICE_KEY}; access_token={ACCESS_TOKEN}; _gat=1";
+            string URL_VALUE = "http://send-anywhere.com/web/key/search/" + key.ToString("D6");
+
+            JObject json = null;
+
+            using (var client = new HttpClient())
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL_VALUE);
+                request.ContentType = "application/json; charset=utf-8";
+                request.Method = "POST";
+                request.Headers["Cookie"] = COOKIE_VALUE;
+
+                WebResponse response = null;
+
+                try
+                {
+                    response = await request.GetResponseAsync();
+                }
+                catch (WebException e) //exception (404 Not Found, 403 Forbidden, 401 Unauthorized)
+                {
+                        Dictionary<string, string> json_dict = new Dictionary<string, string>();
+                        json_dict.Add("error", e.Message);
+
+                        json = JObject.Parse(DictionaryToJson(json_dict));
+                        return json.ToObject<Dictionary<string, string>>();
+                }
+
+                var respStream = response.GetResponseStream();
+                respStream.Flush();
+
+                using (StreamReader sr = new StreamReader(respStream))
+                {
+                    string strContent = sr.ReadToEnd();
+                    respStream = null;
+
+                    json = JObject.Parse(strContent);
+                }
+            }
+            return json.ToObject<Dictionary<string, string>>();
+        }
+
+        /// <summary>
+        /// Get download link (url) using key for asynchronous.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<string> GetLinkAsync()
+        {
+            Dictionary<string, string> response = await GetResponseAsync();
+
+            if (response.ContainsKey("error")) //response the error
+                return string.Format("error: {0}", response["error"]);
+
+            return response["weblink"];
+        }
+
+        /// <summary>
+        /// Get download link with verbose (dictionary) using key for asynchronous.
+        /// </summary>
+        /// <returns>Dictionary (with verbose)</returns>
+        public async Task<Dictionary<string, string>> GetLinkWithVerboseAsync()
+        {
+            return await GetResponseAsync();
+        }
+
+        /// <summary>
+        /// Convert Dictionary Type to Json Type.
+        /// </summary>
+        /// <param name="dict"></param>
+        /// <returns></returns>
+        private string DictionaryToJson(Dictionary<string, string> dict)
+        {
+            return JsonConvert.SerializeObject(dict);
+        }
+    }
+}
